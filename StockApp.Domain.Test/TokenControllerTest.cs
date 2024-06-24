227d05ddeec50b36e386cf292bee9cb91da3457c
cesarprando <cesarprandosz@gmail.com>
2024-06-23 21:45:39 -0300

    Adicionar Testes Unitários para o Controlador TokenController

---
+++ StockApp.Domain.Test/TokenControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockApp.API.Controllers;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.Domain.Test
+{
+    public class TokenControllerTest
+    {
+        [Fact]
+        public async Task Login_ValidCredentials_ReturnsToken()
+        {
+            var authServiceMock = new Mock<IAuthService>();
+            var tokenController = new TokenController(authServiceMock.Object);
+
+            authServiceMock.Setup(service => service.AuthenticateAsync(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(new TokenResponseDto
+            {
+                Token = "token",
+                Expiration = DateTime.UtcNow.AddMinutes(60)
+            });
+
+            var userLoginDto = new UserLoginDto
+            {
+                Username = "testuser",
+                Password = "password"
+            };
+
+            var result = await tokenController.Login(userLoginDto) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            Assert.IsType<TokenResponseDto>(result.Value);
+        }
+    }
+}
